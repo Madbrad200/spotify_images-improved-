3b56d6a6df06815be2df14f023cffd76942ba4ee
Madbrad200 <25277258+Madbrad200@users.noreply.github.com>
2022-07-14 21:24:18 +0100

    switched to user_playlist_tracks attribute, adding every track in a playlist
    In the original code, the script stops short once it hits the 100th song. In this code, it grabs every song from a playlist before it returns the listed dictionaries.
    
    This, therefore, ensures you will be able to grab every single image from the playlist (in the original script, this isn't possible if you had a large playlist).
    
    Note: get_artist is still limited.

--- api.py
+++ api.py
@@ -13,19 +13,24 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 
 def get_access():
-  auth = SpotifyClientCredentials(
-    client_id=config.client_id,
-    client_secret=config.client_secret)
-  sp = spotipy.Spotify(auth_manager = auth)
-  return sp
+    # log into Spotify
+    auth = SpotifyClientCredentials(
+        client_id=config.client_id,
+        client_secret=config.client_secret)
+    sp = spotipy.Spotify(auth_manager=auth)
+    return sp
 
 
 def get_artist(url):
-  spotify = get_access()
-  return spotify.artist_albums(artist_id=url, limit=50)
+    spotify = get_access()
+    return spotify.artist_albums(artist_id=url, limit=50)
 
 
 def get_playlist(url):
-  spotify = get_access()
-  return spotify.playlist(url, fields='name,tracks.items.track.album.name,tracks.items.track.album.images', market='US')
-
+    spotify = get_access()
+    play_results = spotify.user_playlist_tracks(url)
+    tracks = play_results['items']
+    while play_results['next']:
+        play_results = spotify.next(play_results)
+        tracks.extend(play_results['items'])
+    return tracks
