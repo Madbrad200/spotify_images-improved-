2fea393f755798d2fa8fbfb98ae040e83e74a1e9
Madbrad200 <25277258+Madbrad200@users.noreply.github.com>
2022-07-14 21:22:01 +0100

    fixing issues
    1. Updated to flake 8
    2. We're using a different Spotipy attribute in this fork, so the dictionary we receive is formatted slightly different.
    3. Folders are named based on the current time.
    4. Added try/except for OSError's (didn't get these for the current code, might be safe to remove)
    5. stopped stripping file names of spaces + lowering, instead just remove illegal characters

--- images.py
+++ images.py
@@ -11,87 +11,89 @@ import api
 import os
 import requests
 from zipfile import ZipFile
+from datetime import datetime
 
 
 def rename(name):
-  return name.lower().replace(' ','_').replace('/','_').replace('[','(').replace(']',')')
+    # remove any illegal characters and strip empty spaces
+    return name.replace("/", "").replace("<", "").replace(">", "").replace(":", "").replace("\"", "").replace("\\", "").replace("|", "").replace("?", "").replace("*", "").strip()
 
 
 def zip_images(directory):
-  zip_this = ZipFile(directory + '.zip', 'w')
-  os.chdir(directory)
-  for root, dirs, files in os.walk(os.getcwd()):
-    for file in files:
-      zip_this.write(file)
-  zip_this.close()
+    zip_this = ZipFile(directory + '.zip', 'w')
+    os.chdir(directory)
+    for root, dirs, files in os.walk(os.getcwd()):
+        for file in files:
+            zip_this.write(file)
+    zip_this.close()
 
 
 def url_to_uri(uri, typeof):
-  offset = uri.find(typeof)
-  return 'spotify:' + uri[offset:].replace('/',':')
+    offset = uri.find(typeof)
+    return 'spotify:' + uri[offset:].replace('/', ':')
 
 
 def get_images(url, directory=None, verbose=False, zip_this=False):
-  typeof = ''
-  results = ''
-  if 'artist' in url:
-    typeof = 'artist'
-    results = api.get_artist(url)
-  elif 'playlist' in url:
-    typeof = 'playlist'
-    results = api.get_playlist(url)
-  if results == '':
-    print("No results found, check URL and try again.")
-    exit(1)
-  else:
-    print(typeof + " found, downloading...")
+    typeof = ''
+    results = ''
+    if 'artist' in url:
+        typeof = 'artist'
+        results = api.get_artist(url)
+    elif 'playlist' in url:
+        typeof = 'playlist'
+        results = api.get_playlist(url)
+    if results == '':
+        print("No results found, check URL and try again.")
+        exit(1)
+    else:
+        print(typeof + " found, downloading...")
 
-  if typeof == 'artist':
-    name = results['items'][0]['artists'][0]['name']
-    results = results['items']
-  elif typeof == 'playlist':
-    name = results['name']
-    results = results['tracks']['items']
-
-  if verbose:
-    print('Name: ' + name + '\nType: ' + typeof)
-    
-  if directory:
-    directory = directory + '/' + rename(name)
-  else:
-    directory = 'results/' + rename(name)
-
-  if not os.path.exists(directory):
-    os.makedirs(directory)
-
-  count = 0
-  pics = []
-
-  for track in results:
     if typeof == 'artist':
-      url = track['images'][0]['url']
-      name = rename(track['name'])
-    elif typeof == 'playlist':
-      url = track['track']['album']['images'][0]['url']
-      name = rename(track['track']['album']['name'])
-      
-    path = directory + '/' + name + '.jpeg'
-    if os.path.exists(path):
-      continue
+        name = results['items'][0]['artists'][0]['name']
+        results = results['items']
 
-    pic = requests.get(url, allow_redirects=True)
+    count = 0 
+    pics = []
+    
+    today = datetime.now()
+    # create folder with current date/time as folder name
+    if directory:
+        directory = directory + '/' + rename(today.strftime('%Y-%m-%d-%H-%M-%S'))
+    else:
+        directory = 'results/' + rename(today.strftime('%Y-%m-%d-%H-%M-%S'))
 
-    if verbose:
-      print(path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-    open(path, 'wb').write(pic.content)
-    count += 1
-    pics.append(pic)
+    for track in results:
+        if typeof == 'artist':
+            url = track['images'][0]['url']
+            name = rename(track['name'])
+        elif typeof == 'playlist':
+            url = track['track']['album']['images'][0]['url']  # grab image url
+            artist_name = rename(track['track']['artists'][0]['name'])  # grab artist name
+            album_name = rename(track['track']['album']['name'])  # grab album name
+            name = f"{artist_name} - {album_name}"  # this will become the name of the file
+     
+        path = directory + '/' + name + '.jpeg'
+        if os.path.exists(path):
+            continue
 
-  print(str(len(pics)) + " saved to " + directory)
+        pic = requests.get(url, allow_redirects=True)
 
-  if zip_this:
-    zip_images(directory)
+        if verbose:
+            print(path)
 
-  return directory
+        try:
+            open(path, 'wb').write(pic.content)
+            count += 1
+            pics.append(pic)
+        except OSError as e:
+            print(f"{e}\nSkipping..")
 
+    print(str(len(pics)) + " saved to " + directory)
+
+    if zip_this:
+        zip_images(directory)
+
+    return directory
