68c7fb16f770d8506cd8096ca2168d53cb1ec3e5
Madbrad200 <25277258+Madbrad200@users.noreply.github.com>
2022-07-14 21:22:27 +0100

    flake8 compliance

--- collage.py
+++ collage.py
@@ -13,58 +13,58 @@ import glob
 from PIL import Image
 import math
 
+
 def calc_size(total):
 
-  grid_size = math.sqrt(total)
+    grid_size = math.sqrt(total)
 
-  rows = 0
-  cols = 0
+    rows = 0
+    cols = 0
 
-  if grid_size.is_integer():
-    rows, cols = (int(grid_size), int(grid_size))
-  else:
-    rows, cols = (math.floor(grid_size), math.ceil(grid_size))
+    if grid_size.is_integer():
+        rows, cols = (int(grid_size), int(grid_size))
+    else:
+        rows, cols = (math.floor(grid_size), math.ceil(grid_size))
 
-  return (rows, cols)
+    return (rows, cols)
 
 
 def make_collage(directory, verbose):
 
-  pics = []
-  if directory:
-    pics = glob.glob(directory + '/*.jpeg')
-  pics = [Image.open(i) for i in pics]
+    pics = []
+    if directory:
+        pics = glob.glob(directory + '/*.jpeg')
+    pics = [Image.open(i) for i in pics]
 
-  if verbose:
-    print("Total unique pictures: " + str(len(pics)))
+    if verbose:
+        print("Total unique pictures: " + str(len(pics)))
 
-  rows,cols = calc_size(len(pics))
+    rows, cols = calc_size(len(pics))
 
-  if verbose:
-    print("Rows: " + str(rows) + "\tCols: " + str(cols))
+    if verbose:
+        print("Rows: " + str(rows) + "\tCols: " + str(cols))
 
-  # create new blank image of size cols * rows
-  collage = Image.new('RGB', (640 * cols, 640 * rows))
-  y_offset = 0
-  count = 0;
+    # create new blank image of size cols * rows
+    collage = Image.new('RGB', (640 * cols, 640 * rows))
+    y_offset = 0
+    count = 0;
   
-  for row in range (0, rows):
-    # create new image the size of one row
-    new_row = Image.new('RGB', (640 * cols, 640))
-    x_offset = 0
+    for row in range(0, rows):
+        # create new image the size of one row
+        new_row = Image.new('RGB', (640 * cols, 640))
+        x_offset = 0
 
-    # fill row image with images
-    for col in range (0, cols):
-      new_row.paste(pics[count], (x_offset,0))
-      count += 1
-      x_offset += 640
+        # fill row image with images
+        for col in range(0, cols):
+            new_row.paste(pics[count], (x_offset, 0))
+            count += 1
+            x_offset += 640
 
-    # paste row into finished product
-    collage.paste(new_row, (0,y_offset))
-    y_offset += 640
+        # paste row into finished product
+        collage.paste(new_row, (0, y_offset))
+        y_offset += 640
 
-  filename = (directory + '.jpeg')
+    filename = (directory + '.jpeg')
 
-  collage.save(filename)
-  print("Collage saved as: " + filename)
-
+    collage.save(filename)
+    print("Collage saved as: " + filename)
